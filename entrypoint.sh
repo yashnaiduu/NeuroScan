898d8dc8e035869c905bdf1f8645f5d915272135
YASH NAIDU <152394598+yashnaiduu@users.noreply.github.com>
2025-10-03 16:24:38 +0530

    Replace Python server with Gunicorn server

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ if [ ! -f "$MP" ]; then
   fi
   curl -fsSL "$MODEL_URL" -o "$MP"
 fi
-exec python server1.py
+exec gunicorn server1:app -w ${WEB_CONCURRENCY:-2} -k gthread --threads ${WEB_THREADS:-4} -t 120 -b 0.0.0.0:${PORT:-8080}
